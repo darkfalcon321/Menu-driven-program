5b0b8aea2e65810141426ee77ea8f8740dd6375c
darkfalcon321 <58114594+darkfalcon321@users.noreply.github.com>
2020-06-20 21:33:10 +0300

    Update a poop.py

--- a poop.py
+++ a poop.py
@@ -1,34 +1,38 @@
 
-#I know ryt now u would be like what is all this. Don't worry
-Cosmic=dict()               #Creates an empty dictionary
-i=1                              #Empty variable for loop
-flag=0
-n=int(input("Enter number of entries"))
-while(i<=n):             
-    username=input("Enter a Username")                #username
-    dob=(input("Enter your DOB"))                    #dob
-    pswrds=input("Enter your Password ")             #password
-    b=(username,dob,pswrds)        #assigning to a variable
-    Cosmic[username]=b
-    i=i+1
-ls=Cosmic.keys()
-for i in ls:
-    print("\n username-",username)
-    z=Cosmic[i]
-    print("username\t","dob\t","pswrds\t")   
-for j in z:
-    print(j,end="\t")
-#########################################################################################
-
+print("1.Admin\n2.teacher\n3.student")
+choice=int(input("who tf are u?"))
+if choice==1:
+    print("Hello master")
+if choice==2:
+    print("good luck with the slaves")
+if choice==3:
+    print("ah ma man whattup yo?")
+print("1,signup \n2,login")
+choice=eval(input("choose em"))
+if choice==1:
+    def signup():
+        username= input("Enter username: ")
+        dob= input("Enter date of birth: ")
+        pass1= input("Enter password: ")
+        pass2 = input("Confirm your password: ")
+        if pass1 != pass2:
+            while pass1 != pass2:
+                print('Passwords does not match')
+                pass1= input("Enter password: ")
+                pass2 = input("Confirm your password: ")
+        else:
+            print('continue')
+if choice==2:
+    username=(input("Enter username :"))
+    password=(input("Enter password :"))
 import mysql.connector
-from mysql.connector import Error
+conn=mysql.connector.connect(host="localhost",user="root",passwd="root",charset='utf8',database="school")
 try:
-    conn=mysql.connector.connect(host="localhost",user="root",passwd="root",charset='utf8',database="school")
     mycursor=conn.cursor()
-    log=("insert into cosmic (name,password,dob)  values(%s,%s,%s)")
-    val=(username,dob,pswrds)
+    log=("insert into login_page (username,dob,password)  values(%s,%s,%s)")
+    val=("username","dob","passwords")
     mycursor.execute(log,val)
     conn.commit()
-    print("Done")
+    print("\n The process has been over")
 except Exception as e:
     print(e)
